3b4088886627cc902f5b085963b5e36ee617c800
Ida Olson <idaolson@gmail.com>
2022-01-18 23:40:01 -0700

    Add create method to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,26 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end 
+
+  def new
+  end 
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      flash[:notice] = "Item saved!" # this will show on the index page if the item was saved
+      redirect_to items_path
+    else
+      flash[:notice] = "Item not created" # this will show on the new page if form entries are incomplete
+      redirect_to new_item_path
+    end 
+  end
+
+  private
+
+  def item_params
+    params.permit(:title, :description, :unit_price, :stock)
+  end 
 end 
 
 
@@ -10,9 +30,6 @@ end
 
 
 
-# index
-# new
-# create
 # show
 # edit
 # update
