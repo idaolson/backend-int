8af0c00fbfc255fa10234080ba55f7fc1c0d6657
Ida Olson <idaolson@gmail.com>
2022-01-18 22:22:54 -0700

    Create tests for Shipment relationships/validations

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,5 @@
 class Shipment < ApplicationRecord
+  has_many :shipment_items
+  has_many :items, through: :shipment_items
+  validates :customer_name, :email, presence: true
 end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Shipment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:shipment_items) }
+    it { should have_many(:items).through(:shipment_items) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of(:customer_name) }
+    it { should validate_presence_of(:email) }
+  end
 end
